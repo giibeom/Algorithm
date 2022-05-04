9d838530458b4cd36b7a069e3028c3a27f42361f
giibeom <kpmyung@gmail.com>
2022-05-04 10:30:40 +0900

    🧑‍💻 : Baekjoon solved
    문제 - 2558번
    Scanner -> BufferReader 사용
    
    Ref : https://www.acmicpc.net/problem/2588

--- Baekjoon/src/입출력과사칙연산/Q2588/Main.java
+++ Baekjoon/src/입출력과사칙연산/Q2588/Main.java
@@ -1,10 +1,24 @@
 package 입출력과사칙연산.Q2588;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
 
+        int a = Integer.parseInt(scanner.readLine());
+        int b = Integer.parseInt(scanner.readLine());
+
+        scanner.close();
+
+        String[] bArr = (b + "").split("");
+
+        for (int i = bArr.length - 1; i >= 0; i--) {
+            System.out.println(Integer.parseInt(bArr[i]) * a);
+        }
+
+        System.out.println(a * b);
     }
 }
