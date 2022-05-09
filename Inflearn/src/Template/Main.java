96594dd13f08d0421704050dd5908155d138df71
giibeom <kpmyung@gmail.com>
2022-05-09 10:32:38 +0900

    :technologist: Inflearn Algorithm (String)
    - 템플릿 세팅
    - 1.문자 찾기

---
+++ Inflearn/src/Template/Main.java
@@ -0,0 +1,20 @@
+package Template;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public int solution(String str, char t) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        string.Q1.Main T = new string.Q1.Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
