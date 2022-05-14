c87353aa318dbb79fed3fc35d4dd6d2fcb9c5877
giibeom <kpmyung@gmail.com>
2022-05-14 22:33:22 +0900

    :technologist: Inflearn (01-05) 풀이 추가
    - 5. 특정 문자 뒤집기

--- Inflearn/src/string/Q5/Main.java
+++ Inflearn/src/string/Q5/Main.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성하세요.
 
 a#b!GE*T@S -> S#T!EG*b@a
+
 */
 
 public class Main {
@@ -38,11 +39,63 @@ public class Main {
         return answer;
     }
 
+    public String solutionToLecture(String str) {
+        String answer = "";
+
+        char[] chars = str.toCharArray();
+        int lt = 0;
+        int rt = chars.length - 1;
+
+        while (lt < rt) {
+            if (!Character.isAlphabetic(chars[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(chars[rt])) {
+                rt--;
+            } else {
+                char tmp = chars[lt];
+                chars[lt] = chars[rt];
+                chars[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+
+        answer = String.valueOf(chars);
+
+        return answer;
+    }
+
+    public String test(String str) {
+        String answer = "";
+        String[] strArr = str.split("");
+        int lt = 0;
+        int rt = strArr.length - 1;
+        while (lt < rt) {
+            boolean isLtAlphabet = strArr[lt].matches("[a-zA-Z]");
+            boolean isRtAlphabet = strArr[rt].matches("[a-zA-Z]");
+
+            if (isLtAlphabet && isRtAlphabet) {
+                String tmp = strArr[lt];
+                strArr[lt] = strArr[rt];
+                strArr[rt] = tmp;
+                lt++;
+                rt--;
+            } else if (!isLtAlphabet) {
+                lt++;
+            } else {
+                rt--;
+            }
+        }
+        answer = String.join("", strArr);
+
+        return answer;
+    }
+
+
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
-        System.out.println(main.solution(br.readLine()));
+        System.out.println(main.test(br.readLine()));
     }
 }
