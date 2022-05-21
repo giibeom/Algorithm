784b8c25a375a948baa84b1a866d4aa110ca2086
giibeom <kpmyung@gmail.com>
2022-05-21 17:38:16 +0900

    :technologist: Inflearn Array(02-01)
    - 1. 큰 수 출력하기

---
+++ Inflearn/src/array/Q1/Main.java
@@ -0,0 +1,30 @@
+package array.Q1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public String solution(int count, String str) {
+        StringBuilder answer = new StringBuilder();
+        String[] split = str.split(" ");
+        answer.append(split[0] + " ");
+
+        for (int i = 1; i < split.length; i++) {
+            if (Integer.parseInt(split[i]) > Integer.parseInt(split[i-1])) {
+                answer.append(split[i] + " ");
+            }
+        }
+        return answer.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        System.out.println(main.solution(count, str));
+    }
+}
