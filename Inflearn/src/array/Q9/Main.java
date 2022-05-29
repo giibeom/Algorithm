1e401e3f98aee55cc0091cb177befa2a5f74178f
giibeom <kpmyung@gmail.com>
2022-05-29 19:17:39 +0900

    :technologist: Inflearn Array(02-09) 풀이추가
    - 9. 격자판 최대합
    - 각 행과 열은 한 for문에서 구하기 가능
       - 행 : arr[i][j]
       - 열 : arr[j][i]
    - 양 대각선은 특징을 이용하여 한 for문에서 구하기 가능
       - 왼쪽 대각선 : arr[i][i]
       - 오른쪽 대각선 : arr[i][length-1-i]
    - Math.max를 이용하여 최대값 구하기

--- Inflearn/src/array/Q9/Main.java
+++ Inflearn/src/array/Q9/Main.java
@@ -1,10 +1,18 @@
 package array.Q9;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
+
+/*
+- 각 행과 열은 한 for문에서 구하기 가능
+   - 행 : arr[i][j]
+   - 열 : arr[j][i]
+- 양 대각선은 특징을 이용하여 한 for문에서 구하기 가능
+   - 왼쪽 대각선 : arr[i][i]
+   - 오른쪽 대각선 : arr[i][length-1-i]
+- Math.max를 이용하여 최대값 구하기
+* */
 public class Main {
     public int solution(int[][] arr, int rowMax) {
         int answer = 0;
@@ -51,9 +59,38 @@ public class Main {
         return answer;
     }
 
+
+    public int solutionInLecture(int length, int[][] inputArr) {
+        int answer = Integer.MIN_VALUE;
+        int rowSum = 0;
+        int columnSum = 0;
+        int leftDiagonalSum = 0;
+        int rightDiagonalSum = 0;
+
+        for (int i = 0; i < inputArr.length; i++) {
+            rowSum = 0;
+            columnSum = 0;
+            for (int j = 0; j < inputArr[i].length; j++) {
+                rowSum += inputArr[i][j];
+                columnSum += inputArr[j][i];
+            }
+            answer = Math.max(answer, rowSum);
+            answer = Math.max(answer, columnSum);
+        }
+
+        for (int i = 0; i < length; i++) {
+            leftDiagonalSum += inputArr[i][i];
+            rightDiagonalSum += inputArr[i][length - 1 - i];
+        }
+        answer = Math.max(answer, leftDiagonalSum);
+        answer = Math.max(answer, rightDiagonalSum);
+
+        return answer;
+    }
+
     public static void main(String[] args) throws IOException {
         Main main = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+/*        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int length = Integer.parseInt(br.readLine());
         int[][] arr = new int[length][length];
         int rowMax = 0;
@@ -71,9 +108,21 @@ public class Main {
             if (rowSum > rowMax) {
                 rowMax = rowSum;
             }
+        }
+        System.out.println(main.solution(arr, rowMax));*/
 
+
+        Scanner sc = new Scanner(System.in);
+        int length = Integer.parseInt(sc.nextLine());
+
+        int[][] inputArr = new int[length][length];
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                inputArr[i][j] = sc.nextInt();
+            }
         }
 
-        System.out.println(main.solution(arr, rowMax));
+        System.out.println(main.solutionInLecture(length, inputArr));
     }
 }
