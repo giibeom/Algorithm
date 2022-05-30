079f2456a3af4a0d9230e8aed8b91ffb21528861
giibeom <kpmyung@gmail.com>
2022-05-30 20:51:31 +0900

    :technologist: Inflearn Array(02-10) 풀이추가
    - 10. 봉우리
    - 미리 체크할 인덱스를 배열로 만들어놓자
       - 배열로 만드는 이유는 만약 상하좌우 뿐만 아니라 대각선까지 봤을경우 그 외적인 추가작업일 경우 일일히 조건문을 추가하기보다는 배열 하나 선언하고 3중 for문 돌리면 됨
       - int[] dx = {-1, 0, 1, 0};
       - int[] dy = {0, 1, 0, -1};

--- Inflearn/src/array/Q10/Main.java
+++ Inflearn/src/array/Q10/Main.java
@@ -24,6 +24,38 @@ public class Main {
         return arr[i][j] > arr[i - 1][j] && arr[i][j] > arr[i + 1][j] && arr[i][j] > arr[i][j - 1] && arr[i][j] > arr[i][j + 1];
     }
 
+
+    /*
+    - 미리 체크할 인덱스를 배열로 만들어놓자
+        - 배열로 만드는 이유는 만약 상하좌우 뿐만 아니라 대각선까지 봤을경우 그 외적인 추가작업일 경우 일일히 조건문을 추가하기보다는 배열 하나 선언하고 3중 for문 돌리면 됨
+        - int[] dx = {-1, 0, 1, 0};
+        - int[] dy = {0, 1, 0, -1};
+    */
+    public int solutionInLecture(int length, int[][] arr) {
+        int answer = 0;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j <= length; j++) {
+                boolean flag = true;
+                for (int k = 0; k < dx.length; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if (nx >= 0 && nx < length && ny >= 0 && ny < length && arr[nx][ny] >= arr[i][j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
