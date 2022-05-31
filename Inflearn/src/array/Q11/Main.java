ed8210187d6201a985a8d6a12a9b5b279a75f200
giibeom <kpmyung@gmail.com>
2022-05-31 18:47:08 +0900

    :technologist: Inflearn Array(02-11)
    - 11. 임시반장 정하기
    - I used 세로우선탐색
    - Lecture used 가로우선탐색

--- Inflearn/src/array/Q11/Main.java
+++ Inflearn/src/array/Q11/Main.java
@@ -3,21 +3,24 @@ package array.Q11;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
     public int solution(int stuNum, int[][] arr) {
-        int answer = 0;
+        int answer = 1;
         int maxCount = 0;
 
         for (int i = 0; i < arr.length; i++) {
             int count = 0;
-
+            ArrayList<Integer> stuList = new ArrayList<>();
             for (int j = 0; j < 5; j++) {
-
                 for (int k = 0; k < stuNum; k++) {
-                    if (arr[i][j] == arr[k][j]) {
-                        count++;
+                    if (i != k && arr[i][j] == arr[k][j]) {
+                        if (stuList.indexOf(k) == -1) {
+                            count++;
+                            stuList.add(k);
+                        }
                     }
                 }
             }
@@ -26,7 +29,31 @@ public class Main {
                 answer = i + 1;
             }
         }
+        return answer;
+    }
 
+    public int solutionInLectureThroughHorizontalSearch(int stuNum, int[][] arr) {
+        int answer = 1;
+        int maxCount = 0;
+
+        for (int i = 0; i < stuNum; i++) {
+            int count = 0;
+            ArrayList<Integer> stuList = new ArrayList<>();
+            for (int j = 0; j < stuNum; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (i != k && arr[i][j] == arr[k][j]) {
+                        if (stuList.indexOf(k) == -1) {
+                            count++;
+                            stuList.add(k);
+                        }
+                    }
+                }
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                answer = i + 1;
+            }
+        }
         return answer;
     }
 
@@ -44,6 +71,6 @@ public class Main {
             }
         }
 
-        System.out.println(main.solution(stuNum, arr));
+        System.out.println(main.solutionInLectureThroughHorizontalSearch(stuNum, arr));
     }
 }
