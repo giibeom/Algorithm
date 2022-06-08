6c1d3bd3fd5801378339c9b39b2625d416c18de5
giibeom <kpmyung@gmail.com>
2022-06-08 13:08:35 +0900

    :technologist: Inflearn twoPointers & SlidingWindow Algorithm (03-05)
    - 5. 연속된 자연수의 합
    - 연속된 자연수의 합은 num / 2 + 1 이상으로 확인할 필요가 없다
       - ex. 15 / 2 + 1 = 8
       -     7 + 8 = 15
       -     8 + 9 = 17 : 이미 num값을 넘기 때문
    - I used 투포인터 알고리즘
    - Lecture used 투포인터 알고리즘이지만 for문을 num / 2 + 1 만큼만 돔

---
+++ Inflearn/src/twoPointers_slidingWindow/Q5/Main.java
@@ -0,0 +1,76 @@
+package twoPointers_slidingWindow.Q5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public int solution(int num) {
+        int answer = 0;
+        int sum = 0;
+        int lt = 1;
+
+        for (int i = 1; i < num; i++) {
+            
+            // i를 더하고 증가
+            sum += i;
+
+            if (sum == num) {
+                answer++;
+            }
+
+            // sum이 num보다 크다면 작아질때까지 감소
+            while (sum > num) {
+                sum -= lt++;
+                if (sum == num) {
+                    answer++;
+                }
+            }
+
+        }
+
+        return answer;
+    }
+
+
+    // num / 2 + 1 까지만 돌면 됨
+    public int solutionLecture(int num) {
+        int answer = 0;
+        int sum = 0;
+        int lt = 0;
+        int m = num / 2 + 1;
+
+        int[] arr = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int j = 0; j < m; j++) {
+            sum += arr[j];
+            if (sum == num) {
+                answer++;
+            }
+
+            while (sum > num) {
+                sum -= arr[lt++];
+                if (sum == num) {
+                    answer++;
+                }
+            }
+
+
+        }
+        
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        System.out.println(main.solutionLecture(num));
+    }
+}
