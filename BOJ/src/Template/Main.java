be3a3db9436970809b4b257f36212e7403614afb
giibeom <kpmyung@gmail.com>
2022-06-18 00:08:16 +0900

    :technologist: Baekjoon solved (Q10950)
    Ref : https://www.acmicpc.net/problem/10950

--- BOJ/src/Template/Main.java
+++ BOJ/src/Template/Main.java
@@ -5,8 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    public int solution() {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
