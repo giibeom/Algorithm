30cf28df913989fc9e7cc3a76c07a4079deccc75
giibeom <kpmyung@gmail.com>
2022-07-08 20:07:13 +0900

    :technologist: Baekjoon solved (Q2742)
    Ref : https://www.acmicpc.net/problem/2742

---
+++ BOJ/src/반복문/Q2742/Main.java
@@ -0,0 +1,18 @@
+package 반복문.Q2742;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = num; i > 0; i--) {
+            bw.write(i + "\n");
+        }
+
+        bw.close();
+        br.close();
+    }
+}
